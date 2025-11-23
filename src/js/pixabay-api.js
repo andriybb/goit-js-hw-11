c473f10b407337dbfe207bb35fbc2d25734ef10a
Andriy Baranovich <and123bar@gmail.com>
2025-11-23 21:05:41 +0200

    Remove API key from pixabay-api.js
    Removed the API key for security reasons.

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '53377431-d26a2ee1aab1def8444b1097c'; 
+const API_KEY = ''; 
 
 export function getImagesByQuery(query) {
   const url = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
