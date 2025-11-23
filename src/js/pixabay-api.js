00f5b30e579148dd96f3c1ae65e88ee0b9a5c30a
andriy <and123bar@gmail.com>
2025-11-23 21:50:16 +0200

    add space

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_PIXABAY_KEY; 
 
+
 export function getImagesByQuery(query) {
   const url = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
   
